74e7e5d6393e3be5635e07deff6b15f349313464
hafsadahir <hafsa.dahir@student.moringaschool.com>
2022-08-12 23:34:33 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+let input;
+
+input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener("click",()=>{alert('Finally doing what buttons do.You clicked me.')})
 }
